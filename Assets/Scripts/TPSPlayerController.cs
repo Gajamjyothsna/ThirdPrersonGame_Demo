77c57b749fc6fb6de7f08ca7c1b2048388e1374c
GAJAM JYOTHSNA <96329766+Gajamjyothsna@users.noreply.github.com>
2024-02-07 22:22:14 +0530

    Integarted Player animations and added collision detection & gravity

--- Assets/Scripts/TPSPlayerController.cs
+++ Assets/Scripts/TPSPlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace TPSGame
@@ -8,32 +9,67 @@ namespace TPSGame
     {
         [SerializeField] float playerSpeed = 5f;
         [SerializeField] float rotationSpeed = 500f;
+        [Header("GroundCheck")]
+        [SerializeField] private float groundCheckRadius = .2f;
+        [SerializeField] private Vector3 groundCheckOffset;
+        [SerializeField] private LayerMask groundLayer;
 
+        private bool isGrounded;
+        private float ySpeed;
         private UnityEngine.Quaternion targetRoatation;
+        private CharacterController characterController;
+        private Animator playerAnimator;
+        private TPSCamera cameraController;
 
-
-
-        TPSCamera cameraController;
         private void Awake()
         {
             cameraController = Camera.main.GetComponent<TPSCamera>();
+            playerAnimator = GetComponent<Animator>();
+            characterController = GetComponent<CharacterController>();
         }
         private void Update()
         {
             float horizontalValue = Input.GetAxis("Horizontal");
             float verticalValue = Input.GetAxis("Vertical");
 
-            float moveAmount = Mathf.Abs(horizontalValue + verticalValue); //check for player moving
+            float moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalValue)  + Mathf.Abs(verticalValue)); //check for player moving
             Vector3 movement = (new Vector3(horizontalValue, 0, verticalValue)).normalized; //sets the vector lenght to 1
             var moveDirection = cameraController.PlannarRotation * movement; // To make the player movement in camera direction
 
-            if(moveAmount > 0)
+            GroundCheck();
+            Debug.Log("Ground check" + isGrounded);
+
+            if(isGrounded)
+            {
+                ySpeed = -0.5f; //make sure to strict the player to ground
+            }
+            else
+            {
+                //if player is not grouned
+                ySpeed += Physics.gravity.y * Time.deltaTime; //y speed should be increase 
+            }
+            var velocity = movement * playerSpeed;
+            velocity.y = ySpeed;
+
+            characterController.Move(velocity * Time.deltaTime);
+            if (moveAmount > 0)
             {
                 targetRoatation = Quaternion.LookRotation(moveDirection); //player to move facing towards camera;
-                transform.position += movement * playerSpeed * Time.deltaTime;
             }
            transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, targetRoatation, rotationSpeed * Time.deltaTime); // to rotate smoothly
 
+            playerAnimator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime); //to make the smooth out the animation
+
+        }
+        private void GroundCheck() //check player is grounded or not
+        {
+            //transform relates to player 
+            isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+        }
+        private void OnDrawGizmosSelected() //draw gizmos
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
         }
     }
 }
